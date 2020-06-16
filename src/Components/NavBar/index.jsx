22019f6dabfd7a07fe12528997a3629527a12a7c
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-06-16 11:20:58 +0100

    Nav Layout Done

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './style.scss';
 import { UserContext } from '../../Context/UserContext';
 //import images
@@ -6,13 +6,19 @@ import Logo from '../../images/Logo.png';
 
 const index = (props) => {
 	//let a = useContext(UserContext);
+	//const [user, setUser] = useState(null);
 	return (
 		<div className="NavBar">
-			<div className="NavBar__NotLogged">
+			<nav className="NavBar__NotLogged">
 				<img src={Logo} alt="logo" />
-				feactures pricing Login signup
-			</div>
-			<div className="NavBar__Logged"></div>
+				<ul>
+					<li>Feactures</li>
+					<li>Pricing</li>
+					<li>Login</li>
+					<li>Sign Up</li>
+				</ul>
+			</nav>
+			{/* <div className="NavBar__Logged"></div> */}
 		</div>
 	);
 };
