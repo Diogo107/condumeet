5e4083f975eb56581a30b4528a000b2b53c49d7f
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-06-30 16:13:48 +0100

    Activity List

--- src/Views/Activity/index.jsx
+++ src/Views/Activity/index.jsx
@@ -1,11 +1,58 @@
 import React from 'react';
 import './style.scss';
+//Import Images
+import PdfIcon from './../../assets/images/PdfIcon.png';
+import OptionsIcon from './../../assets/images/OptionsIcon.png';
 
 function Index(props) {
 	return (
 		<div className="Activity__View">
 			<div className="Single__Activity--Activity">
 				<h3>May 20, 2020</h3>
+				<div>
+					<p>4 min ago</p>
+					<div>
+						<h4>You paid a Energy Bill</h4>
+						<div className="Bill__Card--Archive">
+							<img src={PdfIcon} alt="PDF Image" />
+							<div>
+								<h3>Bill Gas Energy.pdf</h3>
+								<div>
+									<p>Jan 16, 2020</p>
+									<p>
+										<small>|</small>
+									</p>
+									<p>14.1Mb</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<button>
+						<img src={OptionsIcon} alt="Options" />
+					</button>
+				</div>
+				<div>
+					<p>4 min ago</p>
+					<div>
+						<h4>You paid a Energy Bill</h4>
+						<div className="Bill__Card--Archive">
+							<img src={PdfIcon} alt="PDF Image" />
+							<div>
+								<h3>Bill Gas Energy.pdf</h3>
+								<div>
+									<p>Jan 16, 2020</p>
+									<p>
+										<small>|</small>
+									</p>
+									<p>14.1Mb</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<button>
+						<img src={OptionsIcon} alt="Options" />
+					</button>
+				</div>
 			</div>
 			<div>calendar</div>
 		</div>
