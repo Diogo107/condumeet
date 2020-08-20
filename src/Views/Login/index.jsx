c81cf13b430931f71b29343813e617b25580bc14
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-08-20 21:13:33 +0100

    Updae

--- src/Views/Login/index.jsx
+++ src/Views/Login/index.jsx
@@ -47,13 +47,13 @@ function Index(props) {
 						<img src={GoogleWhiteIcon} alt="Facebook Icon" />
 						Sign up with Google
 					</button>
-					<GoogleLogin
+					{/* <GoogleLogin
 						clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
 						buttonText="Login"
 						onSuccess={responseGoogle}
 						onFailure={responseGoogle}
 						cookiePolicy={'single_host_origin'}
-					/>
+					/> */}
 				</div>
 				<span>
 					<p></p>
