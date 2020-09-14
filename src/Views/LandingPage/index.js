363b4de7e8c16cdc7d7d33c6d80bf4f7f2a79c2e
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-09-14 15:55:20 +0100

    MailChimp

--- src/Views/LandingPage/index.js
+++ src/Views/LandingPage/index.js
@@ -1,25 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 //import images
 import LandingPageBuilding from './../../assets/images/LandingPageBuilding.png';
 
-function index(props) {
+function Index(props) {
+	const [email, setEmail] = useState();
 	return (
 		<div className="Landing-Page">
 			<section>
 				<div>
-					<h1 className="color-lightpurple">
+					{/* <h1 className="color-lightpurple">
 						Transforming how we handle Condominiums.{' '}
 					</h1>
 					<p>
 						Digital experience for condominium management for a more transparent
 						and collaborative neighborhood while reducing costs.
 					</p>
-					<button className="background-lightgreen">JOIN FOR FREE</button>
+					<button className="background-lightgreen">JOIN FOR FREE</button> */}
+					<div id="mc_embed_signup">
+						<form
+							action="https://gmail.us17.list-manage.com/subscribe/post?u=3629eb921ff4887f897adb5d0&amp;id=778e03d58c"
+							method="post"
+							id="mc-embedded-subscribe-form"
+							name="mc-embedded-subscribe-form"
+							className="validate"
+							target="_blank"
+							novalidate
+						>
+							<div id="mc_embed_signup_scroll">
+								<h2>Subscribe to keep updated</h2>
+								{/* 	<div className="indicates-required">
+									<span className="asterisk">*</span> indicates required
+								</div> */}
+								<div className="mc-field-group">
+									<label for="mce-EMAIL">
+										Email Address <span className="asterisk">*</span>
+									</label>
+									<input
+										type="email"
+										value={email}
+										name="EMAIL"
+										className="required email"
+										id="mce-EMAIL"
+										onChange={(e) => {
+											setEmail(e.target.value);
+										}}
+									/>
+								</div>
+								<div id="mce-responses" className="clear">
+									<div
+										className="response"
+										id="mce-error-response"
+										style={{ display: 'none' }}
+									></div>
+									<div
+										className="response"
+										id="mce-success-response"
+										style={{ display: 'none' }}
+									></div>
+								</div>
+								<div
+									style={{ position: 'absolute', left: '-5000px' }}
+									aria-hidden="true"
+								>
+									<input
+										type="text"
+										name="b_3629eb921ff4887f897adb5d0_778e03d58c"
+										tabindex="-1"
+										value=""
+									/>
+								</div>
+								<div className="clear">
+									<input
+										type="submit"
+										value="Subscribe"
+										name="subscribe"
+										id="mc-embedded-subscribe"
+										className="button"
+									/>
+								</div>
+							</div>
+						</form>
+					</div>
 				</div>
 				<img src={LandingPageBuilding} alt="Landing Page Building" />
 			</section>
-			<section>
+			{/* <section>
 				<h1 className="color-lightpurple">What you can get!</h1>
 				<p>A short name that people caracterizes your condominium.</p>
 			</section>
@@ -50,9 +116,9 @@ function index(props) {
 					</ul>
 					<button className="background-lightpurple">Choose</button>
 				</div>
-			</section>
+			</section> */}
 		</div>
 	);
 }
 
-export default index;
+export default Index;
