5f5ddcf1a4bd1a477ebbf7273d470a18d6088016
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-09-22 12:08:10 +0100

    Heroku

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(
 );
 //This line of code attach React files
 //app.use('/static', express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/condumeet/index', indexRouter);
