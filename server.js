4f3ff7e6f649e2f91d0da3bf6220cc044124a74c
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-09-22 14:35:31 +0100

    Heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const profilesRouter = require('./routes/profiles');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.json());
+/* app.use(express.json());
 app.use(cookieParser());
 app.set('trust proxy', 1); // trust first proxy
 app.use(
@@ -28,7 +28,7 @@ app.use(
 			//secure: process.env.NODE_ENV === 'production'
 		},
 	})
-);
+); */
 //This line of code attach React files
 //app.use(express.static(path.join(__dirname, 'client/build')));
 
